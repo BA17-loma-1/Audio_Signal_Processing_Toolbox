178fd3d22884d9b110fffe7676616218c90ed57b
stockan1 <stockan1@students.zhaw.ch>
2017-02-13 15:35:07 +0100

    Initial commit

---
+++ app/src/main/java/ch/zhaw/bait17/audio_signal_processing_toolbox/MainActivity.java
@@ -0,0 +1,13 @@
+package ch.zhaw.bait17.audio_signal_processing_toolbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
