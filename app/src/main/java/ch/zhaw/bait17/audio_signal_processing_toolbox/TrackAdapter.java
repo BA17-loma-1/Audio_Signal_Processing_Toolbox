98ec6d2310fb4f6ab6e8bee88ecb1cd9c8dbdc10
Rémi Georgiou <tonymontana303@gmail.com>
2017-04-03 07:14:18 +0200

    Android ViewHolder pattern for TrackAdapter

--- app/src/main/java/ch/zhaw/bait17/audio_signal_processing_toolbox/TrackAdapter.java
+++ app/src/main/java/ch/zhaw/bait17/audio_signal_processing_toolbox/TrackAdapter.java
@@ -4,12 +4,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import java.util.ArrayList;
 import ch.zhaw.bait17.audio_signal_processing_toolbox.model.Track;
 
 public class TrackAdapter extends BaseAdapter {
+
+    static class ViewHolderItem {
+        TextView title;
+        TextView artist;
+    }
 
     private ArrayList<Track> tracks;
     private LayoutInflater inflater;
@@ -36,13 +40,24 @@ public class TrackAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.media_list_item, parent, false);
-        TextView title = (TextView) layout.findViewById(R.id.track_title);
-        TextView artist = (TextView) layout.findViewById(R.id.track_artist);
+        // Android ViewHolder Pattern for smoother scrolling
+        ViewHolderItem viewHolder;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.media_list_item, parent, false);
+            viewHolder = new ViewHolderItem();
+            viewHolder.artist = (TextView) convertView.findViewById(R.id.track_artist);
+            viewHolder.title = (TextView) convertView.findViewById(R.id.track_title);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolderItem) convertView.getTag();
+        }
+
         Track track = tracks.get(position);
-        title.setText(track.getTitle());
-        artist.setText(track.getArtist());
-        return layout;
+        if (track != null) {
+            viewHolder.artist.setText(track.getArtist());
+            viewHolder.title.setText(track.getTitle());
+        }
+        return convertView;
     }
 
 }
