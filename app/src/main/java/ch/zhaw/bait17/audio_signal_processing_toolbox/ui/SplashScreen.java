a1588263c48818509f6cc337ba26d7cf31eee4ce
Rémi Georgiou <tonymontana303@gmail.com>
2017-04-22 21:03:44 +0200

    Splash screen with app logo

---
+++ app/src/main/java/ch/zhaw/bait17/audio_signal_processing_toolbox/ui/SplashScreen.java
@@ -0,0 +1,28 @@
+package ch.zhaw.bait17.audio_signal_processing_toolbox.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import ch.zhaw.bait17.audio_signal_processing_toolbox.R;
+
+public class SplashScreen extends Activity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.splash_activity);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                SplashScreen.this.startActivity(mainIntent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+
+}
